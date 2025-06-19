d53818bae751da0192bf545ea5578b7bb70a10df
Svetlana Titovskaia <svetameanssun@gmail.com>
2025-06-19 13:26:58 +0200

    changed module01/04

--- module01/ex04/FileProcessor.cpp
+++ module01/ex04/FileProcessor.cpp
@@ -1,8 +1,20 @@
-#include "FileProcessor.hpp"
-#include <iostream> 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileProcessor.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stitovsk <stitovsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 13:09:15 by stitovsk          #+#    #+#             */
+/*   Updated: 2025/06/19 13:26:01 by stitovsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-bool validateArguments(int argc, char** argv){
-    if (argc != 4){
+#include "FileProcessor.hpp"
+#include <iostream>
+
+bool validateArguments(int argc, char** argv) {
+    if(argc != 4) {
         std::cerr << "Error: Wrong number of arguments.\n";
         std::cerr << "Usage: " << argv[0] << " <filename> <string_to_find> <string_to_replace>\n";
         return (false);
@@ -11,19 +23,20 @@ bool validateArguments(int argc, char** argv){
 }
 
 
-std::string generateOutputFileName(const std::string& inputFileName){
+std::string generateOutputFileName(const std::string& inputFileName) {
     std::string outputFileName = inputFileName;
     outputFileName += ".replace";
     return (outputFileName);
 }
 
-bool checkInOutFiles(std::ifstream& inputFile, std::ofstream& outputFile){
-    if (!inputFile.is_open()){
-        std::cerr << "Error: Could not open input file." <<"\n";
+bool checkInOutFiles(std::ifstream& inputFile, std::ofstream& outputFile) {
+    if(!inputFile.is_open()) {
+        std::cerr << "Error: Could not open input file." << "\n";
+        outputFile.close();
         return (false);
     }
-    if (!outputFile.is_open()){
-        std::cerr << "Error: Could not create/open output file." <<"\n";
+    if(!outputFile.is_open()) {
+        std::cerr << "Error: Could not create/open output file." << "\n";
         inputFile.close();
         return (false);
     }
--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stitovsk <stitovsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 13:09:20 by stitovsk          #+#    #+#             */
+/*   Updated: 2025/06/19 13:26:14 by stitovsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "FileProcessor.hpp"
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 
-    if (validateArguments(argc, argv) == false)
+    if(validateArguments(argc, argv) == false)
         return (1);
 
     std::string inputFileName = argv[1];
@@ -14,27 +26,26 @@ int main(int argc, char **argv){
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
-    std::cout << inputFileName <<"\n"
-            << outputFileName <<"\n"
-            << s1 <<"\n"
-            << s2 <<"\n";
+    std::cout << inputFileName << "\n"
+              << outputFileName << "\n"
+              << s1 << "\n"
+              << s2 << "\n";
 
-    if (s1.empty()) {
+    if(s1.empty()) {
         std::cerr << "Error: String to find (s1) cannot be empty.\n";
         return (1);
     }
 
     std::ifstream inputFile(inputFileName.c_str());
     std::ofstream outputFile(outputFileName.c_str());
-    if (checkInOutFiles(inputFile, outputFile) == false){
+    if(checkInOutFiles(inputFile, outputFile) == false) {
         return (1);
     }
 
     std::string line;
-    while(std::getline(inputFile, line)){
-        size_t currentPos = 0; 
-        while((currentPos = line.find(s1, currentPos)) != std::string::npos) 
-        {
+    while(std::getline(inputFile, line)) {
+        size_t currentPos = 0;
+        while((currentPos = line.find(s1, currentPos)) != std::string::npos) {
             line.erase(currentPos, s1.length());
             line.insert(currentPos, s2);
             currentPos += s2.length();
