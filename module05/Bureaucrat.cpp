8831d23925140ce94af09e4c8e42d4479ab376b4
Svetlana Titovskaia <svetameanssun@gmail.com>
2025-07-13 23:33:35 +0200

    changed Bureaucrat

--- module05/Bureaucrat.cpp
+++ module05/Bureaucrat.cpp
@@ -1,1 +1,35 @@
 #include "Bureaucrat.hpp"
+
+
+const std::string Bureaucrat::getName(void) const{
+    return(_name);
+
+}
+
+int Bureaucrat::getGrade(void) const{
+    return(_grade);
+}
+
+void Bureaucrat::incrGrade(void){
+    _grade--;
+
+}
+
+void Bureaucrat::decrGrade(void){
+    _grade++;
+
+}
+
+void Bureaucrat::GradeTooHighException(void){
+
+}
+
+void Bureaucrat::GradeTooLowException(void){
+
+}
+
+ostream& Bureaucrat::operator<<(ostream& os, const Bureaucrat& burocrat)
+{
+    os << burocrat._name << ", bureaucrat grade " << burocrat._grade << '.' << '\n';
+    return os;
+}
