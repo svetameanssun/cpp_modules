a2f30f29af5c35f00816c8b978751f332f58b1b6
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-16 19:32:37 +0200

    Update main.cpp

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -1,6 +1,6 @@
 #include "Bureaucrat.hpp"
 
-int main(){
+/*int main(){
     //Bureaucrat buroNoName = new Bureaucrat();
     Bureaucrat buroNoName;
     Bureaucrat buroBrad("Brad", 155);
@@ -14,4 +14,47 @@ int main(){
     //std::cout << buroBrad;
 }
 
+#include "Bureaucrat.hpp"
+
+int main() {
+    try {
+        Bureaucrat a("Alina", 2);
+        std::cout << a;
+        a.incrGrade(); // Alina's grade = 1
+        std::cout << a;
+        a.incrGrade(); // throws GradeTooHighException
+        std::cout << a;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    std::cout << "--------------------------" << std::endl;
+
+    Bureaucrat b; //this will not throw any exception, because I set the values here
+    try {
+        //Bureaucrat b("Brad", 150);
+        std::cout << b;
+        b.decrGrade(); //  throws GradeTooLowException
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    std::cout << "--------------------------" << std::endl;
+
+    try {
+        Bureaucrat c("Carlos", 0); //  throws GradeTooHighException in the constructor
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught while creating Carlos: " << e.what() << std::endl;
+    }
+
+    std::cout << "--------------------------" << std::endl;
+
+    try {
+        Bureaucrat d("Diana", 151); //throws GradeTooLowException in the constructor
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught while creating Diana: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
 
