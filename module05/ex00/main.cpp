68666212461733f0466dc39da53de578e518679a
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-16 19:33:42 +0200

    Update main.cpp
    Deleted the old main part

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -1,21 +1,5 @@
 #include "Bureaucrat.hpp"
 
-/*int main(){
-    //Bureaucrat buroNoName = new Bureaucrat();
-    Bureaucrat buroNoName;
-    Bureaucrat buroBrad("Brad", 155);
-    std::cout << buroNoName;
-    std::cout << buroBrad;
-    buroBrad.incrGrade();
-    std::cout << buroBrad;
-    buroNoName.decrGrade();
-    std::cout << buroNoName;
-    //buroBrad.setGrade(-1);
-    //std::cout << buroBrad;
-}
-
-#include "Bureaucrat.hpp"
-
 int main() {
     try {
         Bureaucrat a("Alina", 2);
