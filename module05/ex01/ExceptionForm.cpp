38cb7c440506e7733aafced431038ee6350b2f3c
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-24 15:26:27 +0200

    Update ExceptionForm.cpp

--- module05/ex01/ExceptionForm.cpp
+++ module05/ex01/ExceptionForm.cpp
@@ -1,1 +1,9 @@
+#include "Form.hpp"
 
+const char *Form::GradeTooHighException:: what() const throw(){
+    return ("Grade is too high!");
+}
+
+const char *Form::GradeTooLowException:: what() const throw(){
+    return ("Grade is too low!");
+}
