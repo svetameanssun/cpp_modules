fb35c6c49656c57e93b102e9c3ded54d157aa7ed
Svetlana Titovskaia <svetameanssun@gmail.com>
2025-07-29 21:37:46 +0200

    detele ExceptionForm.cpp and ExceptionBuro.cpp

--- module05/ex01/ExceptionBuro.cpp
+++
@@ -1,9 +0,0 @@
-#include "Bureaucrat.hpp"
-
-const char * Bureaucrat::GradeTooHighException:: what() const throw(){
-    return ("Grade is too high!");
-}
-
-const char *Bureaucrat::GradeTooLowException:: what() const throw(){
-    return ("Grade is too low!");
-}
--- module05/ex01/ExceptionForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "Form.hpp"
-
-const char *Form::GradeTooHighException:: what() const throw(){
-    return ("Grade is too high!");
-}
-
-const char *Form::GradeTooLowException:: what() const throw(){
-    return ("Grade is too low!");
-}
