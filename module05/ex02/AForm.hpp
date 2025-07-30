eb111d251d19c697b6713fa2efe06bc943c4c715
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-30 17:03:20 +0200

    Update AForm.hpp
    Supposedly finished creating AForm. It is an abtract base class that will be used when creating obsects of all derived classes.

--- module05/ex02/AForm.hpp
+++ module05/ex02/AForm.hpp
@@ -1,29 +1,47 @@
-#ifndef FORM_HPP
-#define FORM_HPP
+#ifndef AFORM_HPP
+#define AFORM_HPP
 
 #include <iostream>
 #include <string>
 
 class Bureaucrat; // forward declaration
 
-class Form {
+class AForm {
   public:
-    Form(): _name("FormName"), bool(false), _gradeForSign(150), _gradeForExec(150){};
-    Form(const std::string &name, int gradeSign, int gradeExec);
-    Form(const Form& other);
-    Form&operator=(const Form& other);
-    virtual ~Form();
-    
+    // -- CANONICAL FORM --
+    AForm(const std::string &name, int gradeSign, int gradeExec, const std::string &target);
+    AForm(const AForm& other);
+    AForm&operator=(const AForm& other);
+    virtual ~AForm();
 
-      
-    virtual beSigned(Bureaucrat &buro) = 0;
-      
+    // -- GETTERS --
+    const std::string &getName(void) const;
+    bool isSigned(void) const;
+    const int getGradeSign(void) const;
+    const int getGradeExec(void) const;
+    const std::string &getTarget(void) const;
+
+    // -- FORM FUNCTIONS --
+    virtual void beSigned(Bureaucrat const  &buro) = 0;
+    virtual void execute(Bureaucrat const & executor) const= 0;
+
+    // -- EXCEPTION CLASSES --
+    class GradeTooLowException : public std::exception{
+      public:
+        const char* what() const throw();
+    };
+    class GradeTooHighException: public std::exception{
+      public:
+        const char* what() const throw();
+    };
   private:
     const std::string _name;
     bool _signature;
     const int _gradeSign;
     const int _gradeExec;
+    const std::string _target;
+};
 
-}
+std::ostream & operator<<(std::ostream &os, const AForm &formy);
 
 #endif
