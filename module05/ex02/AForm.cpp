52d83f168c8399ce08301ef74a569e8a32a43ea2
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-30 17:16:47 +0200

    Update AForm.cpp
    Added contructor to AForm.cpp

--- module05/ex02/AForm.cpp
+++ module05/ex02/AForm.cpp
@@ -1,1 +1,7 @@
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
 
+ AForm::AForm(const std::string &name, int gradeSign, int gradeExec, const std::string &target):
+                    _name(name), _gradeSign(gradeSign), _gradeExec(gradeExec), _target(target){
+   _signature = false;
+ }
