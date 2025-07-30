7cf3a77cce077f03e6ac7aacd86d3ce3423e3728
Svetlana Titovskaia <svetameanssun@gmail.com>
2025-07-31 00:17:46 +0200

    Create .cpp-s and .hpp-s for derived classes Created files for derived classes, started developing.

--- module05/ex02/ShrubberyCreationForm.cpp
+++ module05/ex02/ShrubberyCreationForm.cpp
@@ -1,1 +1,7 @@
+#include "ShrubberyCreationForm.hpp"
 
+ShrubberyCreationForm::ShrubberyCreationForm(cosnt std::string &target)
+    : AForm("ShrubberyCreationForm", 145, 137){
+    std::cout << "ShrubberyCreationForm constructed\n"; 
+
+}
--- module05/ex02/ShrubberyCreationForm.hpp
+++ module05/ex02/ShrubberyCreationForm.hpp
@@ -1,1 +1,20 @@
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
 
+#include <iostream>
+#include <string>
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm {
+  public:
+    ShrubberyCreationForm(cosnt std::string &target);
+    ShrubberyCreationForm(const ShrubberyCreationForm& other);
+    ShrubberyCreationForm&operator=(const ShrubberyCreationForm& other);
+    ~ShrubberyCreationForm();
+
+    void beSigned(Bureaucrat const  &buro);
+    void execute(Bureaucrat const & executor) const;
+};
+
+#endif
