2bbcc074b4f4d7ae4c24d26874bd0fdfb5271187
Svetlana Titovskaia <svetameanssun@gmail.com>
2025-07-31 23:03:55 +0200

    Improve main.cpp, correct TRYBLOCK

--- module05/ex02/src/main.cpp
+++ module05/ex02/src/main.cpp
@@ -3,6 +3,40 @@
 
 #ifdef USE_CONSTRUCTOR_TRY_BLOCK
 
+int main(void){
+    Bureaucrat buroA("Alice", 1);
+    Bureaucrat buroB("Bob", 150);
+    ShrubberyCreationForm formHome("home");
+    RobotomyRequestForm formLelo("Lelo");
+    PresidentialPardonForm formHarold("Harold");
+
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    std::cout << CYAN << "\t" << buroA << RESET;
+    std::cout << CYAN << "\t" << buroB << RESET;
+    std::cout << CYAN << "\t" << formHome << RESET;
+    std::cout << CYAN << "\t" << formLelo << RESET;
+    std::cout << CYAN << "\t" << formHarold << RESET;
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    buroA.signForm(formHome);
+    buroB.signForm(formLelo);
+    buroA.signForm(formLelo);
+    buroA.signForm(formHarold);
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    b1.executeForm(formHome);
+    b1.executeForm(formLelo);
+    b1.executeForm(formLelo);
+    b2.executeForm(formHarold);
+    b1.executeForm(formHarold);
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+  return (0);
+}
+
 #else
 int main(void){
   try {
