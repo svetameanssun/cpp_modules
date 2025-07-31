c0c2ac626baf0aecb773359d01089e624ea22486
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-07-31 17:30:04 +0200

    Create main.cpp
    Started editing main.cpp for standard main and main for Construcrtor try block

---
+++ module05/ex02/src/main.cpp
@@ -0,0 +1,45 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+#ifdef USE_CONSTRUCTOR_TRY_BLOCK
+
+#else
+int main(void){
+  try {
+    Bureaucrat buroA("Alice", 1);
+    Bureaucrat buroB("Bob", 150);
+    ShrubberyCreationForm formHome("home");
+    RobotomyRequestForm formLelo("Lelo");
+    PresidentialPardonForm formHarold("Harold");
+
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    std::cout << CYAN << "\t" << buroA << RESET;
+    std::cout << CYAN << "\t" << buroB << RESET;
+    std::cout << CYAN << "\t" << formHome << RESET;
+    std::cout << CYAN << "\t" << formLelo << RESET;
+    std::cout << CYAN << "\t" << formHarold << RESET;
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    buroA.signForm(formHome);
+    buroB.signForm(formLelo);
+    buroA.signForm(formLelo);
+    buroA.signForm(formHarold);
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+
+    b1.executeForm(formHome);
+    b1.executeForm(formLelo);
+    b1.executeForm(formLelo);
+    b2.executeForm(formHarold);
+    b1.executeForm(formHarold);
+    
+    std::cout << "<------------------------------------------------------------------------------------------------------------>" << std::endl;
+  }
+  catch (const std::exception & e){
+    std::cerr << "Exception caught: " << e.what() << "\n";
+  }
+  return (0);
+}
+#endif
