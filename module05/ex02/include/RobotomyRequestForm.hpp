25a2e4fbd94aa24bfd3b8fa8d30609e875427644
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-08-01 17:24:01 +0200

    Update RobotomyRequestForm.hpp
    Edited the header, it is a bit different from the rest of the headers.

--- module05/ex02/include/RobotomyRequestForm.hpp
+++ module05/ex02/include/RobotomyRequestForm.hpp
@@ -1,3 +1,29 @@
 #ifndef ROBOTOMYREQUESTFORM_HPP
 #define ROBOTOMYREQUESTFORM_HPP
+
+#include <iostream>
+#include <string>
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+  public:
+    RobotomyRequestForm(cosnt std::string &target);
+   RobotomyRequestForm(const RobotomyRequestForm& other);//this is not inherited from base class
+    RobotomyRequestForm&operator=(const RobotomyRequestForm& other); //this is not inherited from base class
+    ~RobotomyRequestForm();
+
+    const std::string& getName(void) const;
+
+    void beSigned(Bureaucrat const  &buro);
+    void execute(Bureaucrat const & executor) const;
+    static int getSerialNum(void) const;
+    void incrSerialNum(void);
+
+  private:
+    const std::string &_target;
+    static int _serialNum;
+    
+};
+
 #endif
