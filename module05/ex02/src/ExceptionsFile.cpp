0a4b3c139cb9ea9683da23ec9513496f3c5915ba
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-08-02 20:21:07 +0200

    Update ExceptionsFile.cpp
    changing what() functions to get coherent text.

--- module05/ex02/src/ExceptionsFile.cpp
+++ module05/ex02/src/ExceptionsFile.cpp
@@ -2,26 +2,26 @@
 #include "../include/AForm.hpp"
 
 const char *Bureaucrat::GradeTooHighException::what()const throw(){
-    return ("Burocrat cannot have such a high grade!\n");
+    return ("grade is too high\n");
 }
 
 const char *Bureaucrat::GradeTooLowException::what()const throw(){
-    return ("Burocrat cannot have such a low grade!\n");
+    return ("grade is too low\n");
 }
 
 const char *AForm::GradeTooHighException::what()const throw(){
-     return ("Form cannot have such a high grade!\n");
+     return ("required grade is too high\n");
 }
 
 const char *AForm::GradeTooLowException::what()const throw(){
-     return ("Form cannot have such a low grade!\n");
+     return ("required grade is too low\n");
 }
 
 const char* AForm::PermissionDeniedToSign::what()const throw(){
-    return ("Cannot be signed!\n");
+    return ("bureaucrat's grade is too low to sign\n");
 }
 const char* AForm::PermissionDeniedToExec::what()const throw(){
-    return ("Cannot be executed!\n");
+    return ("bureaucrat's grade is too low to execute\n");
 }
 
 
