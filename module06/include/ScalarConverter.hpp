fc6d829beaa78f2ebb44d28fb43d943081b18d0e
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-08-11 18:12:44 +0200

    Create ScalarConverter.hpp
    Creating class that will contain the actual conversion. I think I will apply static_cast type of casting.

---
+++ module06/include/ScalarConverter.hpp
@@ -0,0 +1,12 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter{
+  public:
+    static void convert(const std::string &literal);
+  private:
+    ScalarConverter(){};
+    ~ScalarConverter(){};
+};
+
+#endif
