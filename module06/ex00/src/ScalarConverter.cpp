c90bf8d168ba14b0c35b831dae56b76fc1814f59
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-08-14 17:30:02 +0200

    Update ScalarConverter.cpp
    in development

--- module06/ex00/src/ScalarConverter.cpp
+++ module06/ex00/src/ScalarConverter.cpp
@@ -1,1 +1,34 @@
+/*class ScalarConverter{
+  public:
+    typedef struct ConvertResult{
+      char c;
+      int i;
+      float f;
+      double d;
+    };
+    static void convert(const std::string &literal);
+  private:
+    ScalarConverter(){};
+    ~ScalarConverter(){};
+};*/
+
 #include "ScalarConverter.hpp"
+#include "LiteralDetectorr.hpp"
+
+void convert(const std::string &literal){
+  ScaralConvert::ConvertResult result;
+  
+  if (LiteralDetector::isChar(literal)){
+      //s
+  }
+  else if (LiteralDetector::isInt(literal)){
+    
+  }
+  else if (LieralDetector::isFloat()){
+    
+  }
+  else if (LiteralDetector::isDouble()){
+    
+  }
+
+
