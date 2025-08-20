d37ff90501d404aeba4ff423d467fda4c71443b6
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-08-20 13:56:20 +0200

    Update LiteralDetector.cpp
    Changed functions isChar, isFloat, isDouble, isInt not only for them to check of which type the parameter introduced is, but also to mark the flags in the ScalarConvert::ConvertResult structure , that will  be processed later in the project.

--- module06/ex00/src/LiteralDetector.cpp
+++ module06/ex00/src/LiteralDetector.cpp
@@ -1,5 +1,6 @@
 #include "../include/LiteralDetector.hpp"
 
+// CONVERT ALL STRING UPPER CASE TO LOWER CASE 
 void strToLow(std::string &str){
   int ind = 0;
   for(; ind < (int)str.length(); ++ind){
@@ -9,18 +10,22 @@ void strToLow(std::string &str){
   }
 }
 
-bool LiteralDetector::isChar(std::string str){
+// ----- IS CHAR -----
+bool LiteralDetector::isChar(std::string str,  ScalarConverter::convRes &result){
   if (str.length() == 1 && !std::isdigit(str.at(0))){
+    result.c = literal[0];
     return true;
   }
   if (str.length() == 3 && str.at(0) == '\''
       && str.at(2) == '\'' && !std::isdigit(str.at(1))){
+    result.c = literal[1];
     return true;
   }
   return false;
 }
 
-bool LiteralDetector::isInt(std::string str){
+// ----- IS INT -----
+bool LiteralDetector::isInt(std::string str, ScalarConverter::convRes &result){
     int index = 0;
     int strLen = str.length();
     long int myInt;
@@ -37,18 +42,24 @@ bool LiteralDetector::isInt(std::string str){
     if (myInt > (long)std::numeric_limits<int>::max() || myInt < (long)std::numeric_limits<int>::min()){
         return (false);
     }
+   // STR TO INT
+    result.i = atoi(literal.c_str());
+    result.intFlag = true;
     return true;
 }
 
-bool LiteralDetector::isFloat(std::string str){
+// ----- IS FLOAT -----
+bool LiteralDetector::isFloat(std::string str, ScalarConverter::convRes &result){
 
     int countDots = 0;
     int dotPos = 0;
     int strLen = str.length();
     long double inputNbr;
     
+    result.floatFlag = NULL; 
     strToLow(str);
     if (str == "nanf" || str == "inff" || str == "+inff" || str == "-inff"){
+      result.floatFlag = str;
       return true;
     }
     
@@ -77,19 +88,25 @@ bool LiteralDetector::isFloat(std::string str){
     if (inputNbr > std::numeric_limits<float>::max() || inputNbr < std::numeric_limits<float>::min()){
         return (false);
     }
+     // STR TO FLOAT
+    str.erase(literal.length() - 1, 1);
+    result.f = std::atof(str.c_str()); // I have to print 'f' after this one!
     return (true);
 }
 
-bool LiteralDetector::isDouble (std::string str){
+// ----- IS DOUBLE -----
+bool LiteralDetector::isDouble (std::string str, ScalarConverter::convRes &result){
     int countDots = 0;
     int dotPos = 0;
     int strLen = str.length();
     long double inputNbr;
-    
+
+    result.doubleFlag = NULL;
     strToLow(str);
     int i = 0;
     
     if (str == "nan" || str == "inf" || str == "+inf" || str == "-inf"){
+      result.doubleFlag = str;
       return true;
     }    
     if (str.at(i) == '-'|| str.at(i) == '+'){
@@ -105,7 +122,6 @@ bool LiteralDetector::isDouble (std::string str){
             return (false);
         }
     }
-
     if (countDots != 1 || dotPos == 0){
         return (false);
     }
@@ -113,6 +129,10 @@ bool LiteralDetector::isDouble (std::string str){
     if (inputNbr > std::numeric_limits<double>::max() || inputNbr < std::numeric_limits<double>::min()){
         return (false);
     }
+    // STR TO DOUBLE
+    std::stringstream ss(str);
+    result.d  = 0.0;
+    ss >> result.d;
     return (true);
 }
 
