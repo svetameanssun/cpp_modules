c23165078af70a7a46adaade26031227573123fe
svetameanssun <svetameanssun@gmail.com>
2025-08-27 20:23:16 +0200

    started module06/ex02

---
+++ module06/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void){
+
+}
+//It randomly instantiates A, B, or C and returns the instance as a Base pointer. Feel free
+//to use anything you like for the random choice implementation.
+void identify(Base* p){
+
+}
+//It prints the actual type of the object pointed to by p: "A", "B", or "C".
+void identify(Base& p){
+	
+}
+//t prints the actual type of the object referenced by p: "A", "B", or "C". Using a pointer
+//inside this function is forbidden.
+
+int main(){
+
+}
