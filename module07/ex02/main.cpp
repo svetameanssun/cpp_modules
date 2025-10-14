383582c79ddce085ecb0db567684e3aa8aa4eec6
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-10-14 14:00:49 +0200

    Update main.cpp

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -5,4 +5,10 @@ int main(){
 	int * a = new int();
 	std::cout << *a;
 
+	Array * array00 = new Array();
+	std::cout << *array00;
+	arra
+	Array * array01 = new Array(10);
+	std::cout << *array01;
+	
 }
