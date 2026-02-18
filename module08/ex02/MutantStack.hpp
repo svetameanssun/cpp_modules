ab88f96440e8b0f754705524ef726b002cd2a715
svetameanssun <svetameanssun@gmail.com>
2026-02-18 19:45:33 +0100

    ex02

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -4,18 +4,17 @@
 #include <cstdlib>
 
 template <typename T>
-class MutantStack: public std::stack<T>{
-	public:
-		MutantStack() : std::stack<T>{};
-		MutantStack(const &MutantStack){};
-		MutantStack &operator=(const &MutantStack);
-		~MutantStack();
-		iterator begin();
-		iterator end();
+class MutantStack : public std::stack<T> {
+public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
 
-	private:
-		typedef typename std::stack<T>::container_type::iterator iterator;
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+    MutantStack &operator=(const MutantStack &other) {
+        std::stack<T>::operator=(other);
+        return *this;
+    }
 
-
-
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
 };
