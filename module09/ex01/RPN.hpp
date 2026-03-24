bdf2d741a92026b8057f77cf1876f1436f39326e
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-03-24 13:10:00 +0100

    Define RPN class with constructors and destructor

--- module09/ex01/RPN.hpp
+++ module09/ex01/RPN.hpp
@@ -1,1 +1,21 @@
+#ifndef RPN_HPP
+#define RPN_HPP
 
+#include <iostream>
+#include <string>
+#include <stack>
+
+class RPN {
+  public:
+    RPN();
+    RPN(const std::string &str);
+    RPN(const RPN &other);
+    RPN & operator=(const RPN &other);
+    ~RPN();
+  private:
+    int result;
+    const std::string& inputStr; 
+    std::stack<int> numbersStack;
+};
+
+#endif
