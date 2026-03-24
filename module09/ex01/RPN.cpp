38ec2dff8a9a6955b7ec409c45d4a2e20f401900
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-03-24 13:46:31 +0100

    Implement RPN class with constructors and assignment operator

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -1,1 +1,25 @@
+#include "RPN.cpp"
 
+RPN::RPN() : result(0), inputStr(""), numberStack(){}
+
+RPN::RPN(const std::string &str): result(0), inputStr(str), numberStack(){}
+
+// Copy Constructor
+// Note: You don't check for self-assignment in a constructor 
+// because the object is just being born; it can't be itself yet.
+RPN::RPN(const RPN &other) 
+    : result(other.result), inputStr(other.inputStr), numbersStack(other.numbersStack) {
+}
+
+// Assignment Operator
+RPN & RPN::operator=(const RPN &other) {
+    // Check for self-assignment (e.g., a = a)
+    if (this != &other) {
+        this->result = other.result;
+        this->inputStr = other.inputStr;
+        this->numbersStack = other.numbersStack;
+    }
+    return *this;
+}
+
+RPN::~RPN() {}
