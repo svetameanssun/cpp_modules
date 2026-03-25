b0a8de7f0a347568d10923045b3d948dd69dae2b
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-03-25 17:57:03 +0100

    Refactor RPN class and fix input handling
    Refactor RPN class to fix variable names and improve error handling.

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -1,8 +1,8 @@
-#include "RPN.cpp"
+#include "RPN.hpp"
 
-RPN::RPN() : result(0), inputStr(""), numberStack(){}
+RPN::RPN() : result(0), inputStr(""), numbersStack(){}
 
-RPN::RPN(const std::string &str): result(0), inputStr(str), numberStack(){}
+RPN::RPN(const std::string &str): result(0), inputStr(str), numbersStack(){}
 
 // Copy Constructor
 // Note: You don't check for self-assignment in a constructor 
@@ -24,30 +24,77 @@ RPN & RPN::operator=(const RPN &other) {
 
 RPN::~RPN() {}
 
+bool RPN::isOperator(char c){
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
 void RPN::run(char **argv){
-    RCN rcnCalculator;
+    RCN rpnCalculator;
     if (argv.size() > 1){
         for (int i = 0; i < argv.size(); i++){
             std::string aux = argv[i]; 
-            rcnCalculator.inputStr = rcnCalculator.inputStr + " " + aux;
+            rpnCalculator.inputStr = rpnCalculator.inputStr + " " + aux;
         }
     }
     else if (argv.size == 1){
-         rcnCalculator.inputStr = aux;
+         rpnCalculator.inputStr = aux;
     }
     else{
-        std::cerr << "Error: not enough args"; 
+        std::cerr << "Error: not enough args" << std::endl; 
         return;
     }
-    int i = 0;
-    while (i < rcnCalculator.inputStr.length()){
-        if (rcnCalculator.inputStr.at(i) == ' '){
+   
+    for (int i = 0; i < rcnCalculator.inputStr.length(); i++){
+        if (i == rpnCalculator.inputStr.length() - 1){
+            if( numbersStack.size() == 1){
+                std::cout << "The calculation result is " << this->result << std::endl
+            }
+            else{
+                std::cerr << "Error: wrong input" << std::endl; 
+                return;
+            }
+        }
+        if (rpnCalculator.inputStr.at(i) == ' '){
             continue;
         }
-        strd::stringstream ss(rcnCalculator.inputStr);
-        int num
-        if (isdigit()
-        i++;
+        if (isdigit(rpnCalculator.inputStr.at(i))){
+            std::sting auxString = rpnCalculator.inputStr.substr(i)
+            std::stringstream ss(auxString);
+            int auxNum;
+            ss >> auxNum;
+            if (auxNum > 9 || auxNum < 0){
+                std::cerr << "Error: too big of a number" << std::endl; 
+                return ;
+            }
+            numbersStack.push(auxNum);
+        }
+        else if (isOperator(rpnCalculator.inputStr.at(i)){
+            if (numbersStack.size() < 2){
+                std::cerr << "Error: wrong input" << std::endl; 
+                return;
+            }
+        
+            this->result = numbersStack.top();
+            numbersStack.pop();
+            if (c == '/'){
+                this->result = numbersStack.top() / this->result;
+            }
+            if (c == '-'){
+                this->result = numbersStack.top() - this->result;
+            }
+            if (c == '+'){
+                this->result = numbersStack.top() + this->result;
+            }
+            if (c == '*'){
+                this->result = numbersStack.top() * this->result;
+            }
+            numbersStack.pop();
+            numbersStack.push(this->result);
+            } 
+        }
+        else{
+            std::cerr << "Error: wrong input" << std::endl; 
+            return;
+        }
     }
-    
 }
