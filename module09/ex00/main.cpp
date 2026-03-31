0194b7b5639157e0b063e909079dd55d402b3fcd
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-03-31 13:09:40 +0200

    Improve error messages for file input handling
    Updated error handling for command line arguments.

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,28 +1,13 @@
-
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv){
-	//Temporal solution, later -> exception
-	try{
-		
-
 	if (argc == 1){
-		throw BitcoinException("could not open file.");
-		//std::cerr << "Error: could not open file.\n";
-		//return (1);
+		std::cerr << "Error: could not open file.\n";
+		return (666);
 	}
-	} catch (const std::exception &e){
-		std::cerr << "Fatal error: " << e.what() << std::endl;
-		retrun (1);
-	}
-	try{
 	if (argc > 2){
-		throw BitcoinException("too many args.")
-		//std::cerr << "Error: too many args.\n";
-		//return (1);
-	} catch (const std::exception& e){
-		std::cerr << "Fatal error: " << e.what() << std::endl;
-		return (1);
+		std::cerr << "Error: too many args.\n";
+		return (666);
 	}
 	BitcoinExchange btc("data.csv");
 	btc.run(argv[1]);
