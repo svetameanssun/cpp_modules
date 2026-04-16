b6058290e19bf049379aac8b7e9b4202d5339cff
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-04-16 20:57:00 +0200

    Update PmergeMe.cpp

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -1,1 +1,22 @@
+// Jacobsthal Sequence
+//J(n) = J(n-1) + 2 * J(n-2)
 
+
+  void jacobSeq(std::vector<int> & seq, int seqSize){
+    if (seqSize == 0){
+       return ; 
+    }
+    seq.push_back(0); 
+    if (seqSize == 1){
+      return ;
+    }
+    seq.push_back(1); 
+    if (seqSize == 2){
+      return;
+    }
+    int i = 2;
+    while(i < seqSize){
+      seq.at(i) = seq.at(i - 1) + 2 * seq.at(i - 2);
+      i++;
+    }
+}
