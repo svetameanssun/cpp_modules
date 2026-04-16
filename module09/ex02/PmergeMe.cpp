1bb94c15d837ed06ffca81d8aea7e48c3fd8ebab
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-04-16 21:07:02 +0200

    Refactor jacobSeq function parameter and logic

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -2,21 +2,21 @@
 //J(n) = J(n-1) + 2 * J(n-2)
 
 
-  void jacobSeq(std::vector<int> & seq, int seqSize){
-    if (seqSize == 0){
+  void jacobSeq(std::vector<int> & seq, int pendSize){
+    if (pendSize == 0){
        return ; 
     }
     seq.push_back(0); 
-    if (seqSize == 1){
+    if (pendSize == 1){
       return ;
     }
     seq.push_back(1); 
-    if (seqSize == 2){
+    if (pendSize == 2){
       return;
     }
     int i = 2;
-    while(i < seqSize){
-      seq.at(i) = seq.at(i - 1) + 2 * seq.at(i - 2);
+    while(i < pendSize){
+      seq.push_back(seq.at(i - 1) + 2 * seq.at(i - 2));
       i++;
     }
 }
