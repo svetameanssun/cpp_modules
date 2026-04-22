e82eaf15c69f03ab4aa17b2745c979010ebb9383
svetameanssun <svetameanssun@gmail.com>
2026-04-22 22:14:04 +0200

    finished

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -1,1 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <unordered_set>
 
+
+class PmergeMe {
+ public:
+  PmergeMe();
+  PmergeMe(const PmergeMe& other){};
+  PmergeMe& operator=(const PmergeMe& other){return (*this = other);};
+  ~PmergeMe(){};
+
+  int jacobsthal(int n);
+  std::vector<int> buildInsertionOrder(int pend_size); //I save the insertion order the way I want
+  std::vector<int> fordJohnson(std::vector<int> input);
+  std::deque<int> fordJohnson(std::deque<int> input);
+  template <typename T>
+  bool hasRepeats(T &arr, int n) const;
+
+	
+};
