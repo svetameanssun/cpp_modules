9a3e40dc09c31a8eae06adfbc37c6be93093af9c
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-04-23 13:27:49 +0200

    Refactor argument checks in main function

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,14 +1,10 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv){
-	if (argc == 1){
-		std::cerr << "Error: could not open file.\n";
-		return (666);
-	}
-	if (argc > 2){
-		std::cerr << "Error: too many args.\n";
-		return (666);
-	}
+	if (argc != 2) {
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
 	BitcoinExchange btc("data.csv");
 	btc.run(argv[1]);
 	return (0);
