da8415ea18e8671aef9a54002ad98388948e2eb4
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-04-23 13:24:20 +0200

    Refactor PmergeMe class by moving methods to private

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -2,9 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-#include <string>
 #include <ctime>
-#include <sstream>
 #include <set>
 
 
@@ -15,12 +13,17 @@ class PmergeMe {
   PmergeMe& operator=(const PmergeMe& other);
   ~PmergeMe();
 
-  int jacobsthal(int n);
-  std::vector<int> buildInsertionOrder(int pend_size); //I save the insertion order the way I want
   std::vector<int> fordJohnson(std::vector<int> input);
   std::deque<int> fordJohnson(std::deque<int> input);
+
   template <typename T>
   bool hasRepeats(const T &arr) const;
+
+ private:
+  int jacobsthal(int n);
+  std::vector<int> buildInsertionOrder(int pend_size); //I save the insertion order the way I want
+  
+
 
 	
 };
