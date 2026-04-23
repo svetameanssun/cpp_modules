66b74716e17451fb54b6af8f9c6c32684be2f56f
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-04-23 13:11:44 +0200

    Add main function for sorting with PmergeMe
    Implement main function to process input and measure sorting time.

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,1 +1,52 @@
+#include "PmergeMe.hpp"
+#include <set>
 
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+
+    std::vector<int> v;
+    std::deque<int> d;
+
+    for (int i = 1; i < argc; ++i) {
+        int val = std::atoi(argv[i]);
+        if (val <= 0) { // По условию только положительные
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+        v.push_back(val);
+        d.push_back(val);
+    }
+
+    PmergeMe pmm;
+    if (pmm.hasDuplicates(v)) {
+        std::cerr << "Error: duplicates found" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Before: ";
+    for (size_t i = 0; i < v.size(); ++i) std::cout << v[i] << " ";
+    std::cout << std::endl;
+
+    clock_t startV = clock();
+    std::vector<int> resV = pmm.fordJohnson(v);
+    clock_t endV = clock();
+
+    clock_t startD = clock();
+    std::deque<int> resD = pmm.fordJohnson(d);
+    clock_t endD = clock();
+
+    std::cout << "After:  ";
+    for (size_t i = 0; i < resV.size(); ++i) std::cout << resV[i] << " ";
+    std::cout << std::endl;
+
+    double timeV = static_cast<double>(endV - startV) / CLOCKS_PER_SEC * 1e6;
+    double timeD = static_cast<double>(endD - startD) / CLOCKS_PER_SEC * 1e6;
+
+    std::cout << "Time to process a range of " << v.size() << " elements with std::vector : " << std::fixed << std::setprecision(5) << timeV << " us" << std::endl;
+    std::cout << "Time to process a range of " << d.size() << " elements with std::deque  : " << std::fixed << std::setprecision(5) << timeD << " us" << std::endl;
+
+    return 0;
+}
